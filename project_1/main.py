3e8fb232004755a8648b788817dfa421fdda7afd
La6ryczak <lam6ryczak@gmail.com>
2023-11-13 19:20:33 +0100

    Zadanie 1

--- project_1/main.py
+++ project_1/main.py
@@ -3,6 +3,16 @@ import numpy as np
 
 
 def ex_1():
+
+    for i in range(100):
+        if i% 3 == 0: 
+            print("Fizz")
+        elif i%5 ==0:
+            print("Buzz")
+        elif i%5 ==0 and i%3 ==0:
+            print("FizzBuzz")
+        else:
+            print(i)
     pass
 
 def ex_2():
